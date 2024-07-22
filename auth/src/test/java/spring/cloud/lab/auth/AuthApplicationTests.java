f3e4c4506d0d3de277e2808454fcfab35feb5895
jimmy.choi <jimmychoi113510@gmail.com>
2024-07-22 22:51:29 +0800

    basic env

---
+++ auth/src/test/java/spring/cloud/lab/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.lab.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
